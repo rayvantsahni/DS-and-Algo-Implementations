f82b5d046a9da1cee16f3095c10718ea9406e6b4
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-06-28 18:48:49 +0530

    Create Stack_II.py

---
+++ Stack/Python/Stack_II.py
@@ -0,0 +1,40 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+  
+  def get_value(self):
+    return self.value
+
+class Stack:
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.limit = limit
+    self.size = 0
+  
+  def push(self, value):
+    item = Node(value)
+    item.set_next_node(self.top_item)
+    self.top_item = item
+
+  def pop(self):
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      # Decrement the stack size here:
+      self.size -= 1
+      return item_to_remove.get_value()
+    else:
+      print("This stack is totally empty.")
+  
+  def peek(self):
+    if self.size > 0:
+      return self.top_item.get_value()
+    else:
+      print("Nothing to see here!")
