7cbf4cdf6710c6153a1ac2c0a8740546bbdcaf30
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-07-29 22:19:53 +0530

    Update exponential_search.py

--- Searching Algorithms/Python/exponential_search.py
+++ Searching Algorithms/Python/exponential_search.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 29 21:22:42 2020
-
-@author: Rayvant Sahni
-"""
-
 def exponential_search(search_list, left, right, target):
     if search_list[0] == target:
         return "Found at 0"
@@ -16,8 +9,7 @@ def exponential_search(search_list, left, right, target):
     index = binary_search(search_list, i//2, min(i, len(search_list)-1), target) 
     return index
     
-    
-    
+
 def binary_search(search_list, left, right, target):
     if left > right:
         return "Not Found!"
@@ -32,8 +24,7 @@ def binary_search(search_list, left, right, target):
         return binary_search(search_list, middle + 1, right, target)
     
     
-    
-#print(exponential_search([1,2,3,4,5,6,7,8,9,10], 0, 10-1, 4))
+
 
 def main():
     from random import sample
@@ -43,5 +34,6 @@ def main():
     
     print(search_list, end = "\n\n")
     print(exponential_search(search_list, 0, len(search_list)-1, search_value))
+    
     
 main()    
