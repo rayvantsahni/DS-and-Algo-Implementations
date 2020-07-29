e3bc57c6c99c7230e60ef3367e25ed6c8598f578
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-07-29 16:17:44 +0530

    Create binary_search_using_recursion.py

---
+++ Searching Algorithms/Python/binary_search_using_recursion.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 15:50:13 2020
+
+@author: Rayvant Sahni
+"""
+
+def binary_search(search_list, left, right, target):
+    if left > right:
+        return "Not Found!"
+
+    middle = (left + right) // 2
+
+    if search_list[middle] == target:
+        return "Found at " + str(middle)
+    elif search_list[middle] > target:
+        return binary_search(search_list, left, middle - 1, target)
+    else:
+        return binary_search(search_list, middle + 1, right, target)
+
+
+def main():
+    from random import sample
+    
+    search_list = sorted(sample([i for i in range(20)], 10))
+    search_value = 10
+    
+    print(search_list, end = "\n\n")
+    print(binary_search(search_list, 0, len(search_list)-1, search_value))
+    
+main()
