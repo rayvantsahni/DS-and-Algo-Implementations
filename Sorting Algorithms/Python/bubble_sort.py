d9b8d26187576c19f8a77672dfe593e809301ebd
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-12 02:20:22 +0530

    Create bubble_sort.py

---
+++ Sorting Algorithms/Python/bubble_sort.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 14:51:49 2020
+
+@author: Rayvant Sahni
+"""
+
+# =============================================================================
+# Complexity of Bubble sort is originally O((N)*(N-1)) i.e O(N^2)
+# Although it can be reduced to O((N)*(N-1)/2),
+# but still the order does not change from O(N^2).
+# =============================================================================
+
+def bubble_sort(arr):
+    
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1 - i):
+            
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+              
+    return arr
+
+    
+    
+def main():
+    from random import randint, shuffle
+    
+    a = [randint(0, 50) for _ in range(10)]
+    shuffle(a)
+    print("Unsorted:", a, end = "\n\n")
+    print("Sorted:", bubble_sort(a), end = "\n\n")
+    
+
+main()
