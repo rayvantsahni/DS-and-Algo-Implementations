d0552b92fc53308d7753f42d067315985a68ea95
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-14 01:21:23 +0530

    Create insertion_sort.py

---
+++ Sorting Algorithms/Python/insertion_sort.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 00:37:05 2020
+
+@author: Rayvant Sahni
+"""
+# =============================================================================
+# Insertion Sort is Stable.
+# Complexity is O(N^2).
+# =============================================================================
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        
+        j = i
+        while j > 0 and arr[j-1] > arr[j]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+
+    return arr
+
+    
+def main():
+    from random import randint, shuffle
+    
+    a = [randint(0, 50) for _ in range(10)]
+    shuffle(a)
+    print("Unsorted:", a, end = "\n\n")
+    print("Sorted:", insertion_sort(a), end = "\n\n")
+    
+
+main()
