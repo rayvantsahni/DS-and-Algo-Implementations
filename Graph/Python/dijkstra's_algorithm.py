3108eb68b45168ba449dddd75ab146cd9486e237
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-09-15 03:19:42 +0530

    Create dijkstra's_algorithm.py

---
+++ Graph/Python/dijkstra's_algorithm.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 23:33:13 2020
+
+@author: Rayvant Sahni
+"""
+
+from heapq import heappop, heappush
+from math import inf
+
+
+def dijkstras(graph, start):
+    distances = {}
+    
+    for vertex in graph:
+        distances[vertex] = inf
+    distances[start] = 0
+        
+    unexplored = [(0, start)]
+    
+    while unexplored:
+        current_distance, current_node = heappop(unexplored)
+        
+        for adjacent, edge_weight in graph[current_node]:
+            new_distance = current_distance + edge_weight   
+            
+            if new_distance < distances[adjacent]:
+                distances[adjacent] = new_distance
+                heappush(unexplored, (new_distance, adjacent))
+                
+    return distances
+
+
+
+if __name__ == "__main__":
+    
+    graph = {
+            'A': [('B', 10), ('C', 3)],
+            'C': [('D', 2)],
+            'D': [('E', 10)],
+            'E': [('A', 7)],
+            'B': [('C', 3), ('D', 2)]
+            }
+    
+    print("Shortest distances from D -\n", dijkstras(graph, "D"))
