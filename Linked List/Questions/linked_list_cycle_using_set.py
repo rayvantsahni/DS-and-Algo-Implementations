6f3fbc9f4a4f76ef0f14625451e66c9f7cb162c0
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-12-02 21:34:45 +0530

    Update linked_list_cycle_using_set.py

--- Linked List/Questions/linked_list_cycle_using_set.py
+++ Linked List/Questions/linked_list_cycle_using_set.py
@@ -1,11 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# class LinkedListNode:
+#     def __init__(self, value = 0, next = None):
+#         self.value = value
+#         self.next = next
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
+    def containsCycle(self, head):
         if not head or not head.next:
             return False
         
